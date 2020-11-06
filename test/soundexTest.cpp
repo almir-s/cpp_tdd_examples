13757d0637db92be98e64f6c048c48cc6027e96e
Almir Selimovic <almirs@bicomsystems.com>
2020-11-06 13:01:51 +0100

    Add soundexTest class, implement two simple test cases

--- test/soundexTest.cpp
+++ test/soundexTest.cpp
@@ -1,11 +1,31 @@
-#include <chrono>
+#include <gmock/gmock-matchers.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <chrono>
 #include <thread>
 
+using ::testing::Eq;
+using namespace testing;
+
 class Soundex {
+  private:
+  std::string zeroPad(const std::string& word) { return word + "000"; }
+
+  public:
+  std::string encode(const std::string& word) { return zeroPad(word); }
 };
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+class SoundexEncoding : public Test {
+  public:
   Soundex soundex;
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+  ASSERT_THAT(soundex.encode("A"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+  ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
 

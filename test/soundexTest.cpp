e959f0d16611b9dc975b7a492bf6ad5183c7b8a0
Almir Selimovic <almirs@bicomsystems.com>
2020-11-09 14:24:34 +0100

    Implement Soundex class with appropriate tests

--- test/soundexTest.cpp
+++ test/soundexTest.cpp
@@ -1,3 +1,4 @@
+#include <Soundex.h>
 #include <gmock/gmock-matchers.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
@@ -5,16 +6,7 @@
 #include <chrono>
 #include <thread>
 
-using ::testing::Eq;
 using namespace testing;
-
-class Soundex {
-  private:
-  std::string zeroPad(const std::string& word) { return word + "000"; }
-
-  public:
-  std::string encode(const std::string& word) { return zeroPad(word); }
-};
 
 class SoundexEncoding : public Test {
   public:
@@ -29,3 +21,47 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
   ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
 
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+  EXPECT_THAT(soundex.encode("Ab"), Eq("A100"));
+  ASSERT_THAT(soundex.encode("Ax"), Eq("A200"));
+}
+
+TEST_F(SoundexEncoding, IgnoresNonAlphabetics) {
+  ASSERT_THAT(soundex.encode("A#"), Eq("A000"));
+}
+
+TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
+  ASSERT_THAT(soundex.encode("Acdl"), Eq("A234"));
+}
+
+TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
+  ASSERT_THAT(soundex.encode("Dcldb").length(), Eq(4u));
+}
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
+  ASSERT_THAT(soundex.encode("BaAeEiIoOuUhHyYcdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
+  ASSERT_THAT(soundex.encodedDigit('b'), Eq(soundex.encodedDigit('f')));
+  ASSERT_THAT(soundex.encodedDigit('c'), Eq(soundex.encodedDigit('g')));
+  ASSERT_THAT(soundex.encodedDigit('d'), Eq(soundex.encodedDigit('t')));
+
+  ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
+}
+
+TEST_F(SoundexEncoding, UppercasesFirstLetter) {
+  ASSERT_THAT(soundex.encode("abcd"), StartsWith("A"));
+}
+
+TEST_F(SoundexEncoding, IgnoresCaseWhenEncodingConsonants) {
+  ASSERT_THAT(soundex.encode("BCDL"), Eq(soundex.encode("Bcdl")));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateCodesWhen2ndLetterDuplicates1st) {
+  ASSERT_THAT(soundex.encode("Bbcd"), Eq("B230"));
+}
+
+TEST_F(SoundexEncoding, DoesNotCombineDuplicateEncodingSeparatedByVowels) {
+  ASSERT_THAT(soundex.encode("Jbob"), Eq("J110"));
+}

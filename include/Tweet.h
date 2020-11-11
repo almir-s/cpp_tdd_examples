ff66bbb3835ae949f59036174bb748782b9803de
Almir Selimovic <almirs@bicomsystems.com>
2020-11-11 08:19:17 +0100

    Test asserting exceptions with ASERT_STREQ

--- include/Tweet.h
+++ include/Tweet.h
@@ -1,7 +1,24 @@
 #ifndef Tweet_h
 #define Tweet_h
 
+#include <exception>
+#include <stdexcept>
+#include <string>
+
 class Tweet {
+  private:
+  std::string message;
+  std::string user;
+
+  bool isValid(const std::string& user) { return '@' == user[0]; }
+
+  public:
+  Tweet(){};
+  Tweet(const std::string& message, const std::string& user)
+      : message(message), user(user) {
+    if (!isValid(user)) throw std::runtime_error("Invalid username");
+  }
+
 };
 #endif
 
--- test/retweetCollectionTest.cpp
+++ test/retweetCollectionTest.cpp
@@ -1,7 +1,13 @@
 #include <RetweetCollection.h>
+#include <exception>
 #include <gmock/gmock-matchers.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+#include <memory>
+#include <stdexcept>
+
+#include "Tweet.h"
 
 using namespace ::testing;
 
@@ -9,6 +15,20 @@ class ARetweetCollection : public Test {
   public:
   RetweetCollection collection;
 };
+
+class ARetweetCollectionWithOneTweet : public Test {
+  public:
+  RetweetCollection collection;
+  void SetUp() override { collection.add(Tweet()); }
+};
+
+TEST_F(ARetweetCollectionWithOneTweet, HasSizeOfOne) {
+  ASSERT_THAT(collection.size(), Eq(1u));
+}
+
+TEST_F(ARetweetCollectionWithOneTweet, IsNoLongerEmpty) {
+  ASSERT_FALSE(collection.isEmpty());
+}
 
 TEST_F(ARetweetCollection, IsEmptyWhenCreated) {
   ASSERT_TRUE(collection.isEmpty());
@@ -18,9 +38,14 @@ TEST_F(ARetweetCollection, HasSizeZeroWhenCreated) {
   ASSERT_THAT(collection.size(), Eq(0u));
 }
 
-TEST_F(ARetweetCollection, IsNoLongerEmptyAfterTweetAdded) {
+TEST_F(ARetweetCollection, HasSizeOfOneAfterTweetAdded) {
   collection.add(Tweet());
   ASSERT_THAT(collection.size(), Eq(1u));
+}
+
+TEST_F(ARetweetCollection, IsNoLongerEmptyAfterTweetAdded) {
+  collection.add(Tweet());
+  ASSERT_FALSE(collection.isEmpty());
 }
 
 TEST_F(ARetweetCollection, DecreasesSizeAfterRemovingTweet) {
@@ -33,4 +58,19 @@ TEST_F(ARetweetCollection, IsEmptyAfterRemovingTweet) {
   collection.add(Tweet());
   collection.remove(Tweet());
   ASSERT_TRUE(collection.isEmpty());
+}
+
+TEST_F(ARetweetCollection, RequireUserToStartWithAnAtSign) {
+  std::string invalidUser("invalidUserName");
+  ASSERT_THROW(Tweet t("ultra cool message", invalidUser), std::runtime_error);
+}
+
+TEST_F(ARetweetCollection, RequireUserToStartWithAnAtSignDifferentImpl) {
+  std::string invalidUser("invalidUserName");
+  try {
+    Tweet t("coll msg", invalidUser);
+    FAIL();
+  } catch (const std::runtime_error& err) {
+    ASSERT_STREQ("Invalid username", err.what());
+  }
 }
